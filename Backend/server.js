827a0e56967bb359e47cb7d8653d327fb9dbf5e9
Priyank Shukla <umeshusha1974@gmail.com>
2026-07-10 10:51:39 +0530

    feat: implement product management routes and controller, add multer for image uploads

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/UserRoute.js";
+import productRouter from "./routes/productRoute.js";
 
 dotenv.config();
 
@@ -22,6 +23,9 @@ app.use(cors());
 
 //api endpoints
 app.use("/api/user", userRouter);
+app.use('/api/product',productRouter)
+
+
 app.get('/',(req,res)=>{
     res.send("API Working")
 })
