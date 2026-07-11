41b7f9c9d9344712d257a91764e3d06d93711cc9
Priyank Shukla <umeshusha1974@gmail.com>
2026-07-11 19:31:37 +0530

    feat: enhance Cloudinary integration for image uploads in product management

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -2,11 +2,17 @@ import {v2 as cloudinary} from "cloudinary";
 
 const connectCloudinary = async () => {
 
-    cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret:process.env.CLOUDINARY_SECRET_KEY,
-    })
+        console.log("Cloud Name:", process.env.CLOUDINARY_NAME);
+    console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+    console.log("API Secret:", process.env.CLOUDINARY_API_SECRET ? "Loaded" : "MISSING");
+    console.log("API Secret length:", process.env.CLOUDINARY_API_SECRET.length);
+    console.log("Config check:", cloudinary.config())
+
+   cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME?.trim(),
+    api_key: process.env.CLOUDINARY_API_KEY?.trim(),
+    api_secret: process.env.CLOUDINARY_API_SECRET?.trim(),
+});
 
 }
 
