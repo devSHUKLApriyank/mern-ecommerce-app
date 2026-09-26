ccb6901b27060839ca875442a8d73d3c29bec420
Priyank Shukla <umeshusha1974@gmail.com>
2026-09-26 17:52:22 +0530

    feat: implement cart functionality with add, update, and retrieval endpoints

--- Backend/server.js
+++ Backend/server.js
@@ -24,7 +24,7 @@ app.use(cors());
 //api endpoints
 app.use("/api/user", userRouter);
 app.use('/api/product',productRouter)
-
+app.use('/api/cart',cartRouter)
 
 app.get('/',(req,res)=>{
     res.send("API Working")
