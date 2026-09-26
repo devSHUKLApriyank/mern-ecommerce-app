55ae205ddf772f50c2891242454eecea27122a6e
Priyank Shukla <umeshusha1974@gmail.com>
2026-09-26 18:54:28 +0530

    feat: refactor updateCart and getUserCart functions for improved error handling and code clarity

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -31,14 +31,37 @@ const addToCart = async (req, res) => {
 const updateCart = async (req, res) => {
     try {
 
-        const
+        const { userId, itemId, size, quantity } = req.body
+
+        const userData = await user.Model.findById(userId)
+        let cartData = await userData.cartData;
+
+        cartData[itemId][size] = quantity
+
+        await userModel.findByIdAndUpdate(userId, { cartData })
+        res.json({ success: true, message: " Cart Updated" })
 
     } catch (error) {
-
+        console.log(error)
+        res.json({ success: false, message: error.message })
     }
 }
 // Get user cart data
 const getUserCart = async (req, res) => {
+
+    try {
+
+        const { userId } = req.body
+
+        const userData = await user.Model.findById(userId)
+        let cartData = await userData.cartData;
+
+        res.json({success: true, cartData})
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 
 }
 
